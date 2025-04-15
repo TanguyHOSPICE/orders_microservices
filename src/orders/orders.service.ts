648484755b1895a8db3980c7d03c4535bb5adba5
Ht <htcvhs971@gmail.com>
2025-04-15 14:59:01 +0200

    Update orders service import path

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Order } from '../schemas/order.schema';
+import { Order } from './schemas/order.schema';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
